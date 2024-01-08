b1c89121c388f9a7b483527c3606f9e4d2212b11
Ygor Garofalo <igorgarofalo@gmail.com>
2024-01-08 13:42:03 +0100

    Initial commit

---
+++ src/main/java/ygorgarofalo/BEU2W1D1/BeU2W1D1Application.java
@@ -0,0 +1,13 @@
+package ygorgarofalo.BEU2W1D1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeU2W1D1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeU2W1D1Application.class, args);
+	}
+
+}
